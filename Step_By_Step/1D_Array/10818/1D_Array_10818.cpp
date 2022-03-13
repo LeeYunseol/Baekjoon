41c90835baa6bf576ae95adb4a2e2df512ede0ec
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-14 00:30:54 +0900

    Add files via upload

---
+++ Step_By_Step/1D_Array/10818/1D_Array_10818.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+#include <algorithm>
+int main() {
+    int size;
+    cin >> size;
+    
+    int array[1000000];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+
+    sort(array, array + size);
+
+    cout << array[0] << " " << array[size - 1];
+
+    return 0;
+}
---
+++ Step_By_Step/1D_Array/10818/1D_Array_10818_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    int* array = new int[size]; // 배열의 동적할당 이 형태는 잘 외워두는게 좋을듯?
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    int max = -10000000;
+    int min = 10000000;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (max < array[i])
+            max = array[i];
+        if (min > array[i])
+            min = array[i];
+    }
+    cout << min << " " << max;
+    delete[] array;
+    return 0;
+}
+
+
