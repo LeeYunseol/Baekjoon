978d95b3a5093c0aaf39f2f509fed2ccb4e1b5f2
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-15 00:15:57 +0900

    Add files via upload

---
+++ Step_By_Step/1D_Array/3052/1D_Array_3052.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    // 배열을 0으로 초기화하는 방법
+    int array[42] = {};
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> num;
+        int remain = num % 42;
+        array[remain]++;
+    }
+    int count=0;
+    for (int i = 0; i < sizeof(array) / sizeof(int); i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    cout << count;
+}
