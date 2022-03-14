adeca365fc9988b91554e70c6396acd090d0d85b
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-14 15:36:35 +0900

    Add files via upload

---
+++ Step_By_Step/1D_Array/2562/ID_Array_2562.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int array[9];
+	int max = 0;
+	int index;
+
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> array[i];
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		if (max < array[i])
+		{
+			max = array[i];
+			index = i;
+		}
+			
+	}
+
+	cout << max << endl;
+	cout << index+1;
+}
