229f9351d930ac52159a3917f5604ca6b8b4ab33
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-15 15:40:39 +0900

    Add files via upload

---
+++ Step_By_Step/1D_Array/8958/1D_Array_8958.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    int num;
+    cin >> num;
+    
+    
+    for (int i = 0; i < num; i++)
+    {
+        int sum = 0;
+        int count = 0;
+        string answer;
+        cin >> answer;
+        for (char c : answer)
+        {
+            if (c == 'O')
+            {
+                ++count;
+                sum += count;
+            }
+            else
+                count = 0;
+        }
+        cout << sum << endl;
+    }
+    
+}
+
