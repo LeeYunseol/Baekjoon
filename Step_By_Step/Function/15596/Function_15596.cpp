8b4f8b848b3129a5dc7c5850529053dc38011740
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-17 21:26:27 +0900

    Add files via upload

---
+++ Step_By_Step/Function/15596/Function_15596.cpp
@@ -0,0 +1,10 @@
+﻿#include <vector>
+long long sum(std::vector<int>& a) {
+    long long ans = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        // vector의 index 접근 방법은 이것도 있고 a.at(i) 도 있음.
+        ans += a[i];
+    }
+    return ans;
+}
