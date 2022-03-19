d9ac176b9b58198db28a5ab93e7058ce28ec2aad
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-19 19:11:29 +0900

    Add files via upload

---
+++ Step_By_Step/String/2675/String_2675.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int how_many;
+	cin >> how_many;
+	// 처음에 틀린 이유는 output이 string이어야함.
+	for (int i = 0; i < how_many; i++)
+	{
+		int num;
+		string word;
+		string output;
+		cin >> num >> word;
+		for (char c : word)
+		{
+			for (int i = 0; i < num ; i++)
+			{
+				output += c;
+			}
+		}
+		// 계속 틀렸던 이유는 endl 안해줘서 틀렸던 거임
+		cout << output<<endl;
+	}
+
+	return 0;
+}
