f4818071f0681f69cf5c5f69281916cd65421750
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-19 19:09:57 +0900

    Add files via upload

---
+++ Step_By_Step/String/11720/String_11720.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int size;
+	string num;
+
+	cin >> size >> num;
+
+	int sum = 0;
+	for(int i = 0;  i< num.length() ; i++)
+	{
+		sum += num[i] - '0';
+	}
+	cout << sum;
+	return 0;
+}
