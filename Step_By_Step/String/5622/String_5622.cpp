1bc84dcbb86f37a97d69065b369677d32f78ef88
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-29 13:49:55 +0900

    Add files via upload

---
+++ Step_By_Step/String/5622/String_5622.cpp
@@ -0,0 +1,29 @@
+﻿// 이 문제 읽으면서 가장 처음에 든 생각이 그럼 전화번호에 0이 있으면 어떡하지..?
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string array[8] = { "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+	string input;
+	cin >> input;
+
+	int sum = 0;
+
+	for (int i = 0; i < input.length(); i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			string temp = array[j];
+			for (int k = 0; k < temp.length(); k++)
+			{
+				if (input[i] == temp[k])
+					sum += j + 3;
+			}
+		}
+	}
+	cout << sum;
+	return 0;
+}
