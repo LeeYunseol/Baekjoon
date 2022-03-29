86a5ecb9171f068f724b1f3a5e31d5949d990fed
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-29 13:49:14 +0900

    Add files via upload

---
+++ Step_By_Step/String/2908/String_2908.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void Swap(string& num);
+
+int main()
+{
+	string num1, num2;
+	cin >> num1 >> num2;
+
+	Swap(num1);
+	Swap(num2);
+	//  문자열 string을 int형으로 형변환하기 위하여 stoi (string to integer)을 사용하였다.
+ 
+	int num_1 = stoi(num1);
+	int num_2 = stoi(num2);
+	if (num_1 < num_2)
+		cout << num_2;
+	else
+		cout << num_1;
+	return 0;
+}
+
+void Swap(string& num)
+{
+	char temp = num[0];
+	num[0] = num[2];
+	num[2] = temp;
+}
