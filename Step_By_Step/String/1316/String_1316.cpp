f446ba2d7c017c93100872fd8470eac0dca00a35
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-03-29 13:51:55 +0900

    Add files via upload

---
+++ Step_By_Step/String/1316/String_1316.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool Check_group(string word);
+
+int main()
+{
+	int how_many;
+	cin >> how_many;
+	int count = 0;
+	for (int i = 0; i < how_many; i++)
+	{
+		string input;
+		cin >> input;
+		if (Check_group(input) == true)
+			count++;
+	}
+
+	cout << count;
+
+	return 0;
+}
+
+bool Check_group(string word)
+{
+	int array[26] = { 0 };
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (array[word[i] - 97] == 1)
+			return false;
+
+		else if (i != word.length() - 1 && word[i] != word[i + 1])
+			array[word[i] - 97] = 1;
+	}
+	return true;
+}
