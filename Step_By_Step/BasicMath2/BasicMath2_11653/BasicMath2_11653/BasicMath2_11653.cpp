3f3096cedde10cce781423d9a3a4b54f9eba7648
LeeYunseol <97109063+LeeYunseol@users.noreply.github.com>
2022-06-02 17:39:08 +0900

    Add files via upload

---
+++ Step_By_Step/BasicMath2/BasicMath2_11653/BasicMath2_11653/BasicMath2_11653.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+void Function(int& num);
+
+int main()
+{
+	int num;
+	cin >> num;
+	Function(num);
+
+	return 0;
+}
+
+void Function(int& num)
+{
+	for (int i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			num /= i;
+			cout << i << endl;
+			Function(num);
+		}
+	}
+}
